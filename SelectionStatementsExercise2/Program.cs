f8953cb340708054580744101514fc20eab7a356
Riyard20 <kamarudeenriyad2@gmail.com>
2024-11-11 22:27:26 -0600

    Exercise2 done

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
         }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "Math":
+                    Console.WriteLine("i dont prefer math");
+                    break;
+                case "pe":
+                case "p.e":
+                case "physical education":
+                    Console.WriteLine("P.E is fun");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my favorite too");
+                    break;
+                case "geography":
+                    Console.WriteLine("I like to study the world too");
+                    break;
+                    
+                default:
+                    Console.WriteLine("I like to study the world too");
+                    break;
+                    
+            }
+
+
+        }
+        
     }
 }
